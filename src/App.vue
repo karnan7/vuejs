85feaf07404bfdb5d63429a3df482a6bce6224cb
karnan7 <adithkayalad@gmail.com>
2023-06-12 21:36:25 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+
+import TodoList from './components/todoList.vue';
+
+export default {
+  name: 'App',
+  components :{
+    TodoList
+  }
+}
+</script>
+
+<style>
+body{
+  background: rgb(66, 66, 66);
+  color: aliceblue;
+}
+</style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>Hello Todo List</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todos: [
+                { todoString: "go to gym", completed: true },
+                { todoString: "find some to mall", completed: false },
+                { todoString: "publish that video", completed: false },
+                { todoString: "get a job", completed: true },
+            ]
+        }
+    },
+    methods: {
+        addTodo(newTodo){
+            this.todos.push({
+                todoString: newTodo,
+                completed: false
+            })
+        },
+
+        toggleTodo(todo){
+            todo.completed = !todo.completed
+        },
+
+        editTodo(todo, newTodoString){
+            todo.todoString = newTodoString
+        },
+        deleteTodo(deleteTodo){
+            this.todos = this.todos.filter((todo) => todo !== deleteTodo)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
